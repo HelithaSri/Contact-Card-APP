b12c397f85dd90f54d3db8456f0166970eff42c5
HelithaSri <helitha.pravin@gmail.com>
2023-03-11 22:34:48 +0530

    Done

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/images/dp.png"),
+            ),
+            Text(
+              "Helitha Sri",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: "MarckScript",
+              ),
+            ),
+            Text(
+              'Full Stack Developer',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                fontFamily: "CutiveMono",
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 250.0,
+              child: Divider(
+                color: Colors.blue.shade200,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                visualDensity: VisualDensity.comfortable,
+                minLeadingWidth: 20.0,
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.blueAccent.shade400,
+                  size: 20.0,
+                ),
+                title: Text(
+                  "+94767059199",
+                  style: TextStyle(
+                      color: Colors.blueAccent.shade400,
+                      fontSize: 17.0,
+                      fontFamily: "CutiveMono",
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                visualDensity: VisualDensity.comfortable,
+                minLeadingWidth: 20.0,
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.blueAccent.shade400,
+                  size: 20.0,
+                ),
+                title: Text(
+                  "helitha.pravin@gmail.com",
+                  style: TextStyle(
+                      color: Colors.blueAccent.shade400,
+                      fontSize: 17.0,
+                      fontFamily: "CutiveMono",
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+/*
+Padding(
+padding: EdgeInsets.all(10.0),
+child: Row(
+children: [
+Icon(
+Icons.phone,
+color: Colors.blueAccent.shade400,
+size: 20.0,
+),
+SizedBox(
+width: 10.0,
+),
+Text(
+"+94767059199",
+style: TextStyle(
+color: Colors.blueAccent.shade400,
+fontSize: 17.0,
+fontFamily: "CutiveMono",
+fontWeight: FontWeight.bold),
+),
+],
+),
+),*/
